e28a39997289615fa23587d1619398f1831a5981
quochd <huynhducquoc1995@gmail.com>
2023-06-22 19:46:50 +0700

    first commit

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,32 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getUserId } from '../utils';
+import { getTodos as getTodos} from '../../helpers/todos';
+
+// TODO: Get all TODO items for a current user
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const userId = getUserId(event)
+    const todos = await getTodos(userId)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
